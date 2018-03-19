6b729d6473c96a964cf58a45cd9d79da76bb9021
giannis20012001 <i.tsantilis@yahoo.com>
2018-03-19 21:24:30 +0200

    feat: create traefikapigateway component

---
+++ components/traefikapigateway/src/main/java/eu/arcadia/maestro/traefikapigateway/TraefikApiGatewayTest.java
@@ -0,0 +1,14 @@
+package eu.arcadia.maestro.traefikapigateway;
+
+/**
+ * Created by John Tsantilis
+ * (i[dot]tsantilis[at]yahoo.com A.K.A lumi) on 1/2/2017.
+ */
+
+public class TraefikApiGatewayTest {
+    public static void main(String[] args) {
+        //
+
+    }
+
+}
